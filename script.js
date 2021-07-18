448597d6e842943b39117d5fba8fa8b35302f8f1
Islaster <52678788+Islaster@users.noreply.github.com>
2021-07-17 17:33:08 -0700

    fixed bugs

--- script.js
+++ script.js
@@ -1,25 +1,37 @@
 'use strict';
 let count = 0;
-
-const btns = document.querySelectorAll('.btn');
+const decreaseQs = document.querySelector('.decrease');
+const increaseQs = document.querySelector('.increase');
 const value = document.querySelector('h2');
-btns.forEach((btn) => {
-  btn.addEventListener('click',function (e) {
-    const styles = e.currentTarget.classList;
-    if(styles.contains('decrease')){
-      count--;
-    }else if(styles.contains('increase')){
-      count++;
-    }else{
-      count = 0;
-    }
-    if(count > 0){
-      value.style.color = 'green';
-    }else if(count < 0){
-      value.style.color = 'red';
-    }else{
-      value.style.color = 'black';
-    }
-    value.textContent = count;
-  })
+decreaseQs.addEventListener('click', function () {
+  count--;
+  if(count < 0){
+    value.style.color = 'red'
+  }
+  if(decreaseQs.style.color === 'white'){
+    decreaseQs.style.color = 'red';
+  }else{
+    decreaseQs.style.color = 'white';
+  }
+
+  value.textContent = count;
+});
+increaseQs.addEventListener('click', function () {
+  count++;
+  if(count > 0){
+    value.style.color = 'green';
+  }
+  if(increaseQs.style.color === 'white'){
+    increaseQs.style.color = 'green';
+  }else{
+    increaseQs.style.color = 'white';
+  }
+  value.textContent = count;
+});
+document.querySelector('.reset').addEventListener('click',function () {
+  value.textContent = 0;
+  count = 0;
+  value.style.color = 'black';
+  decreaseQs.style.color = 'white';
+  increaseQs.style.color = 'white';
 });
